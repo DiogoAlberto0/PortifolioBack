33670f3fc53083fe4bf4b9b5858190dcea96c8d3
DiogoAlberto0 <dafgo03@Diogo>
2023-10-25 16:15:26 -0300

    fixed errors

--- prisma/migrations/20231012225617_project_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Project" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "githubUrl" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
-    "projectUrl" TEXT
-);
