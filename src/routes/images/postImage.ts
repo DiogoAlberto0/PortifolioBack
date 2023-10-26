18bd1f7c7b08ca401d9f23f2e1b511002c954313
DiogoAlberto0 <dafgo03@Diogo>
2023-10-26 09:06:50 -0300

    connecting vercel blob

--- src/routes/images/postImage.ts
+++ src/routes/images/postImage.ts
@@ -5,46 +5,55 @@ import fs from 'node:fs'
 import util from 'node:util'
 import { pipeline } from "node:stream";
 import { verifyJwt } from "../../verifyjwt";
+import { put } from "@vercel/blob";
 
 const pump = util.promisify(pipeline)
 
 
 export const postImage = (server: FastifyInstance) => {
 
-    
+
     server.register(multipart);
-    
-    server.post('/image', async ( req, res ) => {
+
+    server.post('/image', async (req, res) => {
         const { auth, message, decoded } = await verifyJwt(req.headers.authentication as string)
-        
-        if(!auth) return res.status(401).send(message)
 
-        if(decoded?.admin === false) return res.status(401).send('Você não tem permissão para cadastrar um projeto')
+        if (!auth) return res.status(401).send(message)
 
-        const data = await req.file({limits: {
-            fileSize: 1000000 * 10,  // For multipart forms, the max file size in bytes
-            files: 2,                // Max number of file fields
-        }});
-        
+        if (decoded?.admin === false) return res.status(401).send('Você não tem permissão para cadastrar um projeto')
+
+        const data = await req.file({
+            limits: {
+                fileSize: 1000000 * 10,  // For multipart forms, the max file size in bytes
+                files: 2,                // Max number of file fields
+            }
+        });
+
         //verificações do arquivo
-        if(!data) return res.send('Selecione um arquivo de imagem!');
-        
+        if (!data) return res.send('Selecione um arquivo de imagem!');
+
         const input = data.mimetype
         const barra = input.indexOf('/')
         const dataType = input.slice(0, barra)
-        
-        if(dataType != 'image') return res.send('Selecione um arquivo de imagem!')
-        
-        if(data.file.truncated) res.send(new server.multipartErrors.FilesLimitError());
+
+        if (dataType != 'image') return res.send('Selecione um arquivo de imagem!')
+
+        if (data.file.truncated) res.send(new server.multipartErrors.FilesLimitError());
         //nome unico do arquivo baseado na data em milesegundos
         const name = `${Date.now()}-${data.filename}`;
-        
+
         //salvando a imagem no servidor
 
-            // image url
-            const imagePath = `tmp/${name}`
-        await pump(data.file, fs.createWriteStream(imagePath))
-        .then(() => res.send({path: imagePath}))
-        .catch(( err ) => res.send( err ))
+        // image url
+        // const imagePath = `tmp/${name}`
+        // await pump(data.file, fs.createWriteStream(imagePath))
+        //     .then(() => res.send({ path: imagePath }))
+        //     .catch((err) => res.send(err))
+
+        const blob = await put(name, data.file, {
+            access: 'public',
+        });
+
+        return res.send({ path: blob.url })
     });
 }
